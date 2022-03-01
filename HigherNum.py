d5dbdfc3f90c6726b6a462a48d8b91f9dc89915b
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-03-01 13:12:05 +0700

    Update HigherNum.py

--- HigherNum.py
+++ HigherNum.py
@@ -1,17 +1,10 @@
 #O(n) example using sort function to sort array of numbers
 num = [2,5,8,0,3,10,6]
-num.sort()
 
-print(num)
+max = num[0]
 
-print("without sort")
-new_num = []
-while num:
-    min = num[0]  
-    for x in num: 
-        if x < min:
-            min = x
-    new_num.append(min)
-    num.remove(min)    
+for x in num:
+    if x > max:
+        max = x
 
-print(new_num)
+print(max)
