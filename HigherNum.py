8cf1c33e0db013397f8644f714661d3104b97637
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-03-01 13:04:51 +0700

    Update HigherNum.py

--- HigherNum.py
+++ HigherNum.py
@@ -3,3 +3,15 @@ num = [2,5,8,0,3,10,6]
 num.sort()
 
 print(num)
+
+print("without built in function")
+new_num = []
+while num:
+    min = num[0]  
+    for x in num: 
+        if x < min:
+            min = x
+    new_num.append(min)
+    num.remove(min)    
+
+print(new_num)
