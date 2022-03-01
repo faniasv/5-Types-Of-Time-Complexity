dc9433eb9aa142de1c3081be309c39b19f9fc36e
Savilla Tifania Mahadewi <89194313+faniasv@users.noreply.github.com>
2022-03-01 14:29:02 +0700

    Update OddEven.py

--- OddEven.py
+++ OddEven.py
@@ -2,6 +2,6 @@
 num = int (input("Enter a number: "))
 
 if(num % 2) == 0:
-    print(str(num), " is Even")
+    print("{0} is Even".format(num))
 else:
-    print(str(num), " is Odd")
+    print("{0} is Odd".format(num))
